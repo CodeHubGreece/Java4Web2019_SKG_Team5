d6d6b8d6a47bd40b7c3c66b762319e5a94f9a916
panmaroul <panmaroul@gmail.com>
2019-12-07 11:11:11 +0200

    Initial Project Commit

---
+++ src/main/java/org/regeneration/team5/DoctorProject/DoctorProjectApplication.java
@@ -0,0 +1,30 @@
+package org.regeneration.team5.DoctorProject;
+
+import org.regeneration.team5.DoctorProject.entities.User;
+import org.regeneration.team5.DoctorProject.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+public class DoctorProjectApplication implements CommandLineRunner {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorProjectApplication.class, args);
+	}
+	@Autowired
+	public UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		List<User> userList = User.getUserList();
+		userList.add(new User("asd","asdasd","asda","asdsad"));
+		User.setUserList(userList);
+	}
+}
---
+++ src/main/java/org/regeneration/team5/DoctorProject/entities/User.java
@@ -0,0 +1,104 @@
+package org.regeneration.team5.DoctorProject.entities;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer userID;
+    @Column
+    private String firstname;
+    @Column
+    private String lastname;
+    @Column
+    private String username;
+    @Column
+    private String password;
+    @Column
+    private Integer property = 0;
+    @OneToOne(mappedBy = "user")
+    private Citizen citizen;
+
+    private static List<User> userList;
+
+
+
+    public void User(){
+
+    }
+//    public void User(String username,String password){
+//        this.username = username;
+//        this.password = password;
+//    }
+
+//    public void User(String firstname,String lastname, String username, String password){
+//        this.firstname = firstname;
+//        this.lastname = lastname;
+//        this.password = password;
+//        this.username = username;
+//    }
+
+    public User(String firstname, String lastname, String username, String password){
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.password = password;
+        this.username = username;
+    }
+
+    public Integer getUserID() {
+        return userID;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Integer getProperty() {
+        return property;
+    }
+
+    public void setUserID(Integer userID) {
+        this.userID = userID;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setProperty(Integer property) {
+        this.property = property;
+    }
+
+    public static List<User> getUserList() {
+        return userList;
+    }
+    public static void setUserList(List<User> userList){
+        userList = userList;
+    }
+}
