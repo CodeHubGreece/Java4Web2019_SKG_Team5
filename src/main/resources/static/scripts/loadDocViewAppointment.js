9db5fbf48061c9a33976eada26cd0ef4e008a17b
Giannisp1995 <giannisp1995@gmail.com>
2019-12-19 20:12:27 +0200

    Doctor pages JS finished

--- src/main/resources/static/scripts/loadDocViewAppointment.js
+++ src/main/resources/static/scripts/loadDocViewAppointment.js
@@ -2,53 +2,6 @@ $(document).ready(function () {
     let url_string = window.location.href;
     let url1 = new URL(url_string);
     let id = url1.searchParams.get("appointmentid");
-    //let specialityTitle = url1.searchParams.get("specialityTitle");
-    console.log(specialityTitle,id);
-    // $.ajax({
-    //     url: ROOT_PATH + "/getspeciality",
-    //     type: "GET",
-    //     data:{
-    //         title:specialityTitle
-    //     },
-    //     dataType : "json",
-    //     contentType:"application/json",
-    //     success: function (data) {
-    //         console.log(data);
-    //         $("#specialty").append("<option value='" + data.specialityId + "'>" + data.title + "</option>")
-    //     }
-    // });
-    // $.ajax({
-    //     url: ROOT_PATH + "/citizen/appointment/"+id,
-    //     type: "GET",
-    //     dataType : "json",
-    //     contentType:"application/json",
-    // }).then(function(data) {
-    //     console.log(data);
-    //     let datetime = data.createdAt;
-    //     let dateFor = [];
-    //     let timeFor = [];
-    //     for(var i=0;i<10; i++){
-    //         dateFor[i] = datetime[i];
-    //     }
-    //     for(var j=11;j<16;j++){
-    //         timeFor[j-11] = datetime[j];
-    //     }
-    //     let d = dateFor.join('');
-    //     let t = timeFor.join('');
-    //     console.log(d,t,data.doctor.user["lastname"],data.specialityTitle);
-    //     // const myNode = document.getElementById("specialty");
-    //     // myNode.innerHTML = '';
-    //     $("#specialty").append("<option value='" + id + "'>" + specialityTitle + "</option>")
-    //     //$("select[name=specialty]").val(data.specialityTitle);
-    //     $("#doctor").append("<option value='" + data.doctor.doctorId + "'>" + data.doctor.user["lastname"] + "</option>")
-    //     //$("select[name=doctor]").val(data.doctor.user["lastname"]);
-    //
-    //     $("input[name=Date]").val(d);
-    //     $("input[name=usr_time]").val(t);
-    //
-    //     $("textarea[name=description]").val(data.symptoms);
-    //     $("textarea[name=more_comments]").val(data.info);
-    // });
 
     $.ajax({
         url: ROOT_PATH + "/doctor/appointment/" + id
@@ -76,5 +29,8 @@ $(document).ready(function () {
 });
 
 function viewpatientinfo() {
+    let url_string = window.location.href;
+    let url1 = new URL(url_string);
+    let id = url1.searchParams.get("appointmentid");
     location.replace(ROOT_PATH + "/pages/doctor/citizen_info.html?appointmentid="+id);
 };
--- src/main/resources/static/scripts/loadPatientInfo.js
+++ src/main/resources/static/scripts/loadPatientInfo.js
@@ -3,16 +3,16 @@ $(document).ready(function () {
     let url1 = new URL(url_string);
     let id = url1.searchParams.get("appointmentid");
     //let specialityTitle = url1.searchParams.get("specialityTitle");
-    console.log(specialityTitle,id);
+    //console.log(specialityTitle,id);
 
     $.ajax({
-        url: ROOT_PATH + "/doctor/appointments/" + id
+        url: ROOT_PATH + "/doctor/appointment/" + id
     }).then(function(data) {
-        $("input[name=AMKA]").val(data.date);
-        $("input[name=Firstname]").val(data.time);
-        $("input[name=Lastname]").val(data.time);
-        $("input[name=Email]").val(data.time);
-        $("input[name=Phone]").val(data.time);
+        $("input[name=AMKA]").val(data.citizen.amka);
+        $("input[name=FirstName]").val(data.citizen.user.firstname);
+        $("input[name=LastName]").val(data.citizen.user.lastname);
+        $("input[name=Email]").val(data.citizen.email);
+        $("input[name=Phone]").val(data.citizen.mobile);
     });
 
 });
