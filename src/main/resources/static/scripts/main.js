9e7c34742b4124642d454a1facb62a1d0386a333
panmaroul <panmaroul@gmail.com>
2019-12-20 17:33:54 +0200

    Go back button

--- src/main/resources/static/scripts/main.js
+++ src/main/resources/static/scripts/main.js
@@ -2,6 +2,3 @@ const LOCAL_STORAGE_LOGIN_TOKEN_NAME = "username";
 const LOCAL_STORAGE_LOGIN_TOKEN_ID = "userid";
 const LOCAL_STORAGE_LOGIN_TOKEN_ROLE = "userrole";
 const ROOT_PATH = "http://localhost:8080";
-function goBack() {
-    window.history.back();
-}
